92fc29f6d537e0e34b4ef674c193acee5db6fb8c
eurosoft-uk <attau@eurosoft-uk.com>
2025-06-25 10:53:07 +0000

    ARM- X64 reproducible Dockerfiles working

--- docker-buildaa64.sh
+++ docker-buildaa64.sh
@@ -1,5 +1,5 @@
 # Build Docker image
-docker build -f Dockerfile.aa64 -t shim-repro .
+docker build -f Dockerfile.aa64 -t shim-repro . 2>&1 | tee logs/buildaa64.log
 
 # Run the container and extract the built binary
 docker create --name shim-container shim-repro
